f67e5f15004ac7d6a6a222aa679668dc33dc1160
Map <nkappesm@gmail.com>
2022-03-28 16:16:33 -0300

    added users worker api connection

--- app/workers/users_worker.rb
+++ app/workers/users_worker.rb
@@ -3,4 +3,14 @@ class UsersWorker
 
   def perform(*)
   end
+
+  private
+
+  def api_connection
+    uri = URI("#{ENV['USERS_API_URL']}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Put.new(uri.path, 'Content-Type' => 'application/json')
+    req['APIKEY'] = ENV['USERS_API_KEY']
+    http.request(req)
+  end
 end
